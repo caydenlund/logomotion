84772616742ccd4c315b685647ea361154200dd6
Cayden Lund <caydenlund@gmail.com>
2025-04-16 20:09:51 -0600

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+use std::cell::RefCell;
+
+pub const FN_COLOR: &str = "\x1b[3;33m";
+pub const LOG_COLOR: &str = "\x1b[34m";
+pub const RESET_COLOR: &str = "\x1b[0m";
+pub const INDENT_COLOR: &str = "\x1b[2;37m";
+
+thread_local! {
+    pub static LOG_DEPTH: RefCell<usize> = const { RefCell::new(0) };
+}
+
+/// Prints indentation based on the current depth
+#[macro_export]
+macro_rules! indent {
+    () => {{
+        print!("{}", $crate::INDENT_COLOR);
+        $crate::LOG_DEPTH.with(|d| {
+            (0..*d.borrow()).for_each(|_| print!("⎸    "));
+        });
+        print!("{}", $crate::RESET_COLOR);
+    }};
+}
+
+/// A guard that decreases the indentation level when dropped
+pub struct LogGuard;
+
+impl LogGuard {
+    pub fn new() -> Self {
+        LOG_DEPTH.with(|d| *d.borrow_mut() += 1);
+        LogGuard
+    }
+}
+
+impl Default for LogGuard {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Drop for LogGuard {
+    fn drop(&mut self) {
+        LOG_DEPTH.with(|d| *d.borrow_mut() -= 1);
+        indent!();
+        println!("}}");
+    }
+}
+
+/// Starts a new logging scope
+#[macro_export]
+macro_rules! func {
+    ($($arg:tt)*) => {{
+        $crate::indent!();
+        print!("{}", $crate::FN_COLOR);
+        print!($($arg)*);
+        println!("{} {{", $crate::RESET_COLOR);
+        $crate::LogGuard::new()
+    }};
+}
+
+/// Logs a message within the current scope
+#[macro_export]
+macro_rules! log {
+    ($($arg:tt)*) => {{
+        $crate::indent!();
+        print!("- {}", $crate::LOG_COLOR);
+        print!($($arg)*);
+        println!("{}", $crate::RESET_COLOR);
+    }};
+}
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use logomotive::{func, log};
+
+fn foo() {
+    let ctx = func!("foo()");
+    log!("[foo 1]");
+    bar(1);
+    log!("[foo 2]")
+}
+
+fn bar(a: u32) {
+    let ctx = func!("bar(a: {a})");
+    log!("[bar 1]");
+    baz(1, 2);
+    log!("[bar 2]");
+}
+
+fn baz(a: u32, b: u32) {
+    let ctx = func!("bar(a: {a}, b: {b})");
+    log!("[baz]");
+}
+
+fn main() {
+    let ctx = func!("main()");
+    foo();
+}
